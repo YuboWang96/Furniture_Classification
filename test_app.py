cb6cb090923370c192a27d85caa8b3b3e5521fe3
YuboWang96 <yuboalex.wang@mail.utoronto.ca>
2023-02-23 16:09:48 -0500

    Test files

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+from fastapi.testclient import TestClient
+from app import app
+client = TestClient(app)
+
+def test_read_main():
+
+    file_path = "test_image\Abner PU Swivel Chair.jpg"
+    if file_path:
+        files = open(file_path, 'rb')
+        response = client.post("/",files={"file": files})
+        assert response.status_code == 200
+        print(6)
+    else:
+        pytest.fail("Scratch file does not exists.")
+
+
+test_read_main()
